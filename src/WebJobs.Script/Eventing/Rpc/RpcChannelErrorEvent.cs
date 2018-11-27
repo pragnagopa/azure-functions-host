4cfa8c89654bb452ac894693ebbe49b6013c557e
pragnagopa <pgopa@microsoft.com>
2018-11-27 13:29:31 -0800

    Add RpcChannelErrorEvent

---
+++ src/WebJobs.Script/Eventing/Rpc/RpcChannelErrorEvent.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+
+namespace Microsoft.Azure.WebJobs.Script.Eventing
+{
+    public class RpcChannelErrorEvent : ScriptEvent
+    {
+        internal RpcChannelErrorEvent(string workerId, Exception exception)
+            : base(nameof(WorkerErrorEvent), EventSources.Worker)
+        {
+            WorkerId = workerId ?? throw new ArgumentNullException(nameof(workerId));
+            Exception = exception;
+        }
+
+        internal string WorkerId { get; private set; }
+
+        public Exception Exception { get; private set; }
+    }
+}
