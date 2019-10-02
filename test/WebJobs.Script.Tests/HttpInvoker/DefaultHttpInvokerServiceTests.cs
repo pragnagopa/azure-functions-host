1ba982560336003b2d3480ee796d0148f820f298
pragnagopa <pgopa@microsoft.com>
2019-10-02 14:41:37 -0700

    Add test for SimpleHttpTriggerInvocationRequest

--- test/WebJobs.Script.Tests/HttpInvoker/DefaultHttpInvokerServiceTests.cs
+++ test/WebJobs.Script.Tests/HttpInvoker/DefaultHttpInvokerServiceTests.cs
@@ -6,18 +6,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Net.Http.Formatting;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.Azure.WebJobs.Script.Description;
 using Microsoft.Azure.WebJobs.Script.OutOfProc;
 using Microsoft.Azure.WebJobs.Script.OutOfProc.Http;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.Net.Http.Headers;
 using Moq;
 using Moq.Protected;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using Xunit;
 
 namespace Microsoft.Azure.WebJobs.Script.Tests.HttpInvoker
@@ -60,21 +58,48 @@ namespace Microsoft.Azure.WebJobs.Script.Tests.HttpInvoker
             handlerMock.Protected().Setup<Task<HttpResponseMessage>>("SendAsync",
                 ItExpr.IsAny<HttpRequestMessage>(),
                 ItExpr.IsAny<CancellationToken>())
-                .Callback<HttpRequestMessage, CancellationToken>((request, token) => ValidateRequest(request))
-                .ReturnsAsync(GetValidHttpResponseMessage());
+                .Callback<HttpRequestMessage, CancellationToken>((request, token) => ValidateDefaultInvocationRequest(request))
+                .ReturnsAsync(HttpInvokerTestUtilities.GetValidHttpResponseMessage());
 
             _httpClient = new HttpClient(handlerMock.Object);
             _defaultHttpInvokerService = new DefaultHttpInvokerService(_httpClient, new OptionsWrapper<HttpInvokerOptions>(_httpInvokerOptions), _testLoggerFactory);
-            var testScriptInvocationContext = GetScriptInvocationContext();
+            var testScriptInvocationContext = HttpInvokerTestUtilities.GetScriptInvocationContext(TestFunctionName, _testInvocationId, _testLogger);
             await _defaultHttpInvokerService.ProcessDefaultInvocationRequest(testScriptInvocationContext);
             var invocationResult = await testScriptInvocationContext.ResultSource.Task;
 
-            var expectedHttpScriptInvocationResult = GetTestHttpScriptInvocationResult();
+            var expectedHttpScriptInvocationResult = HttpInvokerTestUtilities.GetTestHttpScriptInvocationResult();
             var testLogs = _testLogger.GetLogMessages();
             Assert.True(testLogs.Count() == expectedHttpScriptInvocationResult.Logs.Count());
             Assert.True(testLogs.All(m => m.FormattedMessage.Contains("invocation log")));
             Assert.Equal(expectedHttpScriptInvocationResult.Outputs.Count(), invocationResult.Outputs.Count());
             Assert.Equal(expectedHttpScriptInvocationResult.ReturnValue, invocationResult.Return);
+        }
+
+        [Fact]
+        public async Task ProcessSimpleHttpTriggerInvocationRequest_Succeeds()
+        {
+            var handlerMock = new Mock<HttpMessageHandler>(MockBehavior.Strict);
+
+            handlerMock.Protected().Setup<Task<HttpResponseMessage>>("SendAsync",
+                ItExpr.IsAny<HttpRequestMessage>(),
+                ItExpr.IsAny<CancellationToken>())
+                .Callback<HttpRequestMessage, CancellationToken>((request, token) => ValidateSimpleHttpTriggerInvocationRequest(request))
+                .ReturnsAsync(HttpInvokerTestUtilities.GetValidSimpleHttpResponseMessage());
+
+            _httpClient = new HttpClient(handlerMock.Object);
+            _defaultHttpInvokerService = new DefaultHttpInvokerService(_httpClient, new OptionsWrapper<HttpInvokerOptions>(_httpInvokerOptions), _testLoggerFactory);
+            var testScriptInvocationContext = HttpInvokerTestUtilities.GetSimpleHttpTriggerScriptInvocationContext(TestFunctionName, _testInvocationId, _testLogger);
+            await _defaultHttpInvokerService.ProcessSimpleHttpInvocationRequest(testScriptInvocationContext);
+            var invocationResult = await testScriptInvocationContext.ResultSource.Task;
+            var expectedHttpResponseMessage = HttpInvokerTestUtilities.GetValidSimpleHttpResponseMessage();
+            var expectedResponseContent = await expectedHttpResponseMessage.Content.ReadAsStringAsync();
+            var testLogs = _testLogger.GetLogMessages();
+            Assert.Equal(0, testLogs.Count());
+            if (invocationResult.Return is HttpResponseMessage response)
+            {
+                Assert.Equal(expectedHttpResponseMessage.StatusCode, response.StatusCode);
+                Assert.Equal(expectedResponseContent, await response.Content.ReadAsStringAsync());
+            }
         }
 
         [Fact]
@@ -86,11 +111,11 @@ namespace Microsoft.Azure.WebJobs.Script.Tests.HttpInvoker
                 ItExpr.IsAny<HttpRequestMessage>(),
                 ItExpr.IsAny<CancellationToken>())
                 .Callback<HttpRequestMessage, CancellationToken>((request, token) => RequestHandler(request))
-                .ReturnsAsync(GetHttpResponseMessageWithJsonContent());
+                .ReturnsAsync(HttpInvokerTestUtilities.GetHttpResponseMessageWithJsonContent());
 
             _httpClient = new HttpClient(handlerMock.Object);
             _defaultHttpInvokerService = new DefaultHttpInvokerService(_httpClient, new OptionsWrapper<HttpInvokerOptions>(_httpInvokerOptions), _testLoggerFactory);
-            var testScriptInvocationContext = GetScriptInvocationContext();
+            var testScriptInvocationContext = HttpInvokerTestUtilities.GetScriptInvocationContext(TestFunctionName, _testInvocationId, _testLogger);
             await _defaultHttpInvokerService.ProcessDefaultInvocationRequest(testScriptInvocationContext);
             var invocationResult = await testScriptInvocationContext.ResultSource.Task;
             Assert.Equal(0, invocationResult.Outputs.Count());
@@ -106,11 +131,11 @@ namespace Microsoft.Azure.WebJobs.Script.Tests.HttpInvoker
                 ItExpr.IsAny<HttpRequestMessage>(),
                 ItExpr.IsAny<CancellationToken>())
                 .Callback<HttpRequestMessage, CancellationToken>((request, token) => RequestHandler(request))
-                .ReturnsAsync(GetHttpResponseMessageWithStringContent());
+                .ReturnsAsync(HttpInvokerTestUtilities.GetHttpResponseMessageWithStringContent());
 
             _httpClient = new HttpClient(handlerMock.Object);
             _defaultHttpInvokerService = new DefaultHttpInvokerService(_httpClient, new OptionsWrapper<HttpInvokerOptions>(_httpInvokerOptions), _testLoggerFactory);
-            var testScriptInvocationContext = GetScriptInvocationContext();
+            var testScriptInvocationContext = HttpInvokerTestUtilities.GetScriptInvocationContext(TestFunctionName, _testInvocationId, _testLogger);
             await _defaultHttpInvokerService.ProcessDefaultInvocationRequest(testScriptInvocationContext);
             await Assert.ThrowsAsync<UnsupportedMediaTypeException>(async () => await testScriptInvocationContext.ResultSource.Task);
         }
@@ -131,7 +156,7 @@ namespace Microsoft.Azure.WebJobs.Script.Tests.HttpInvoker
 
             _httpClient = new HttpClient(handlerMock.Object);
             _defaultHttpInvokerService = new DefaultHttpInvokerService(_httpClient, new OptionsWrapper<HttpInvokerOptions>(_httpInvokerOptions), _testLoggerFactory);
-            var testScriptInvocationContext = GetScriptInvocationContext();
+            var testScriptInvocationContext = HttpInvokerTestUtilities.GetScriptInvocationContext(TestFunctionName, _testInvocationId, _testLogger);
             await _defaultHttpInvokerService.ProcessDefaultInvocationRequest(testScriptInvocationContext);
             await Assert.ThrowsAsync<HttpRequestException>(async () => await testScriptInvocationContext.ResultSource.Task);
         }
@@ -146,11 +171,11 @@ namespace Microsoft.Azure.WebJobs.Script.Tests.HttpInvoker
                 ItExpr.IsAny<HttpRequestMessage>(),
                 ItExpr.IsAny<CancellationToken>())
                 .Callback<HttpRequestMessage, CancellationToken>((request, token) => RequestHandler(request))
-                .ReturnsAsync(GetValidHttpResponseMessage());
+                .ReturnsAsync(HttpInvokerTestUtilities.GetValidHttpResponseMessage());
 
             _httpClient = new HttpClient(handlerMock.Object);
             _defaultHttpInvokerService = new DefaultHttpInvokerService(_httpClient, new OptionsWrapper<HttpInvokerOptions>(_httpInvokerOptions), _testLoggerFactory);
-            _defaultHttpInvokerService.ProcessLogsFromHttpResponse(GetScriptInvocationContext(), httpScriptInvocationResult);
+            _defaultHttpInvokerService.ProcessLogsFromHttpResponse(HttpInvokerTestUtilities.GetScriptInvocationContext(TestFunctionName, _testInvocationId, _testLogger), httpScriptInvocationResult);
             var testLogs = _testLogger.GetLogMessages();
             Assert.True(testLogs.Count() == httpScriptInvocationResult.Logs?.Count());
             if (httpScriptInvocationResult.Logs != null && httpScriptInvocationResult.Logs.Count() > 1)
@@ -159,50 +184,7 @@ namespace Microsoft.Azure.WebJobs.Script.Tests.HttpInvoker
             }
         }
 
-        public ScriptInvocationContext GetScriptInvocationContext()
-        {
-            ScriptInvocationContext scriptInvocationContext = new ScriptInvocationContext()
-            {
-                ExecutionContext = new ExecutionContext()
-                {
-                    InvocationId = _testInvocationId,
-                    FunctionName = TestFunctionName,
-                },
-                BindingData = GetScriptInvocationBindingData(),
-                Inputs = GetScriptInvocationInputs(),
-                ResultSource = new TaskCompletionSource<ScriptInvocationResult>(),
-                Logger = _testLogger,
-                AsyncExecutionContext = System.Threading.ExecutionContext.Capture()
-            };
-            var functionMetadata = new FunctionMetadata
-            {
-                Name = TestFunctionName
-            };
-            var httpTriggerBinding = new BindingMetadata
-            {
-                Name = "req",
-                Type = "httpTrigger",
-                Direction = BindingDirection.In,
-                Raw = new JObject()
-            };
-            var queueInputBinding = new BindingMetadata
-            {
-                Name = "queueInput",
-                Type = "queue",
-                Direction = BindingDirection.In
-            };
-            var httpOutputBinding = new BindingMetadata
-            {
-                Name = "res",
-                Type = "http",
-                Direction = BindingDirection.Out,
-                Raw = new JObject()
-            };
-            scriptInvocationContext.FunctionMetadata = functionMetadata;
-            return scriptInvocationContext;
-        }
-
-        private async void ValidateRequest(HttpRequestMessage httpRequestMessage)
+        private async void ValidateDefaultInvocationRequest(HttpRequestMessage httpRequestMessage)
         {
             Assert.Contains($"{HttpInvokerConstants.UserAgentHeaderValue}/{ScriptHost.Version}", httpRequestMessage.Headers.UserAgent.ToString());
             Assert.Equal(_testInvocationId.ToString(), httpRequestMessage.Headers.GetValues(HttpInvokerConstants.InvocationIdHeaderName).Single());
@@ -212,7 +194,7 @@ namespace Microsoft.Azure.WebJobs.Script.Tests.HttpInvoker
             HttpScriptInvocationContext httpScriptInvocationContext = await httpRequestMessage.Content.ReadAsAsync<HttpScriptInvocationContext>();
 
             // Verify Metadata
-            var expectedMetadata = GetScriptInvocationBindingData();
+            var expectedMetadata = HttpInvokerTestUtilities.GetScriptInvocationBindingData();
             Assert.Equal(expectedMetadata.Count(), httpScriptInvocationContext.Metadata.Count());
             foreach (var key in expectedMetadata.Keys)
             {
@@ -220,7 +202,7 @@ namespace Microsoft.Azure.WebJobs.Script.Tests.HttpInvoker
             }
 
             // Verify Data
-            var expectedData = GetScriptInvocationInputs();
+            var expectedData = HttpInvokerTestUtilities.GetScriptInvocationInputs();
             Assert.Equal(expectedData.Count(), httpScriptInvocationContext.Data.Count());
             foreach (var item in expectedData)
             {
@@ -229,60 +211,21 @@ namespace Microsoft.Azure.WebJobs.Script.Tests.HttpInvoker
             }
         }
 
+        private async void ValidateSimpleHttpTriggerInvocationRequest(HttpRequestMessage httpRequestMessage)
+        {
+            Assert.Contains($"{HttpInvokerConstants.UserAgentHeaderValue}/{ScriptHost.Version}", httpRequestMessage.Headers.UserAgent.ToString());
+            Assert.Equal(_testInvocationId.ToString(), httpRequestMessage.Headers.GetValues(HttpInvokerConstants.InvocationIdHeaderName).Single());
+            Assert.Equal(ScriptHost.Version, httpRequestMessage.Headers.GetValues(HttpInvokerConstants.HostVersionHeaderName).Single());
+            Assert.Equal($"http://localhost:{_defaultPort}/{TestFunctionName}{HttpInvokerTestUtilities.QueryParamString}", httpRequestMessage.RequestUri.AbsoluteUri);
+            Assert.Equal(HttpInvokerTestUtilities.AcceptHeaderValue, httpRequestMessage.Headers.GetValues(HeaderNames.Accept).FirstOrDefault());
+            Assert.Equal(HttpInvokerTestUtilities.UTF8AcceptCharset, httpRequestMessage.Headers.GetValues(HeaderNames.AcceptCharset).FirstOrDefault());
+            var content = await httpRequestMessage.Content.ReadAsStringAsync();
+            Assert.Equal($"\"{HttpInvokerTestUtilities.HttpContentStringValue}\"", content);
+        }
+
         private void RequestHandler(HttpRequestMessage httpRequestMessage)
         {
             //used for tests that do not need request validation
-        }
-
-        private List<(string name, DataType type, object val)> GetScriptInvocationInputs()
-        {
-            List<(string name, DataType type, object val)> inputs = new List<(string name, DataType type, object val)>();
-            inputs.Add(("myqueueItem", DataType.String, "HelloWorld"));
-            return inputs;
-        }
-
-        private Dictionary<string, object> GetScriptInvocationBindingData()
-        {
-            Dictionary<string, object> bindingData = new Dictionary<string, object>();
-            bindingData["dequeueCount"] = 4;
-            bindingData["VisibleTime"] = new DateTime(2019, 10, 1);
-            bindingData["helloString"] = "helloMetadata";
-            return bindingData;
-        }
-
-        private HttpResponseMessage GetValidHttpResponseMessage()
-        {
-            return new HttpResponseMessage(HttpStatusCode.OK)
-            {
-                Content = new ObjectContent<HttpScriptInvocationResult>(GetTestHttpScriptInvocationResult(), new JsonMediaTypeFormatter())
-            };
-        }
-
-        private HttpResponseMessage GetHttpResponseMessageWithStringContent()
-        {
-            return new HttpResponseMessage(HttpStatusCode.OK) { Content = new StringContent("TestMessage") };
-        }
-
-        private HttpResponseMessage GetHttpResponseMessageWithJsonContent()
-        {
-            JObject response = new JObject();
-            response["key1"] = "value1";
-            response["key2"] = "value2";
-            return new HttpResponseMessage(HttpStatusCode.OK) { Content = new StringContent(response.ToString(), System.Text.Encoding.UTF8, "application/json") };
-        }
-
-        private HttpScriptInvocationResult GetTestHttpScriptInvocationResult()
-        {
-            return new HttpScriptInvocationResult()
-            {
-                Logs = new List<string>() { "invocation log1", "invocation log2" },
-                Outputs = new Dictionary<string, object>()
-                {
-                    { "output1", "output1Value" },
-                    { "output2", "output2Value" }
-                },
-                ReturnValue = "Hello return"
-            };
         }
     }
 }
---
+++ test/WebJobs.Script.Tests/HttpInvoker/HttpInvokerTestUtilities.cs
@@ -0,0 +1,200 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Azure.WebJobs.Script.Description;
+using Microsoft.Azure.WebJobs.Script.OutOfProc.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Net.Http.Headers;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Microsoft.Azure.WebJobs.Script.Tests.HttpInvoker
+{
+    public class HttpInvokerTestUtilities
+    {
+        public const string QueryParamString = "?id=149656222&name=Ink%20And%20Toner";
+        public const string UTF8AcceptCharset = "UTF-8";
+        public const string AcceptHeaderValue = "text/plain";
+        public const string HttpContentStringValue = "hello world";
+        public const string HttpResponseContentStringValue = "hello world response";
+
+        public static HttpRequest GetTestHttpRequest()
+        {
+            var httpRequest = new DefaultHttpContext().Request;
+            httpRequest.Method = "GET";
+            httpRequest.Query = GetTestQueryParams();
+            httpRequest.Headers[HeaderNames.AcceptCharset] = UTF8AcceptCharset;
+            httpRequest.Headers[HeaderNames.Accept] = AcceptHeaderValue;
+
+            var json = JsonConvert.SerializeObject(HttpContentStringValue);
+            var stream = new MemoryStream(Encoding.UTF8.GetBytes(json));
+            httpRequest.Body = stream;
+            httpRequest.ContentLength = stream.Length;
+            return httpRequest;
+        }
+
+        public static QueryCollection GetTestQueryParams()
+        {
+            return new QueryCollection(QueryHelpers.ParseQuery(QueryParamString));
+        }
+
+        public static List<(string name, DataType type, object val)> GetSimpleHttpTriggerScriptInvocationInputs()
+        {
+            List<(string name, DataType type, object val)> inputs = new List<(string name, DataType type, object val)>();
+            inputs.Add(("myqueueItem", DataType.String, GetTestHttpRequest()));
+            return inputs;
+        }
+
+        public static HttpScriptInvocationResult GetTestHttpScriptInvocationResult()
+        {
+            return new HttpScriptInvocationResult()
+            {
+                Logs = new List<string>() { "invocation log1", "invocation log2" },
+                Outputs = new Dictionary<string, object>()
+                {
+                    { "output1", "output1Value" },
+                    { "output2", "output2Value" }
+                },
+                ReturnValue = "Hello return"
+            };
+        }
+
+        public static HttpResponseMessage GetHttpResponseMessageWithJsonContent()
+        {
+            JObject response = new JObject();
+            response["key1"] = "value1";
+            response["key2"] = "value2";
+            return new HttpResponseMessage(HttpStatusCode.OK) { Content = new StringContent(response.ToString(), System.Text.Encoding.UTF8, "application/json") };
+        }
+
+        public static Dictionary<string, object> GetScriptInvocationBindingData()
+        {
+            Dictionary<string, object> bindingData = new Dictionary<string, object>();
+            bindingData["dequeueCount"] = 4;
+            bindingData["VisibleTime"] = new DateTime(2019, 10, 1);
+            bindingData["helloString"] = "helloMetadata";
+            return bindingData;
+        }
+
+        public static HttpResponseMessage GetValidHttpResponseMessage()
+        {
+            return new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new ObjectContent<HttpScriptInvocationResult>(GetTestHttpScriptInvocationResult(), new JsonMediaTypeFormatter())
+            };
+        }
+
+        public static HttpResponseMessage GetValidSimpleHttpResponseMessage()
+        {
+            return new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent(HttpResponseContentStringValue)
+            };
+        }
+
+        public static HttpResponseMessage GetHttpResponseMessageWithStringContent()
+        {
+            return new HttpResponseMessage(HttpStatusCode.OK) { Content = new StringContent("TestMessage") };
+        }
+
+        public static List<(string name, DataType type, object val)> GetScriptInvocationInputs()
+        {
+            List<(string name, DataType type, object val)> inputs = new List<(string name, DataType type, object val)>();
+            inputs.Add(("myqueueItem", DataType.String, "HelloWorld"));
+            return inputs;
+        }
+
+        public static ScriptInvocationContext GetScriptInvocationContext(string functionName, Guid invocationId, ILogger testLogger)
+        {
+            ScriptInvocationContext scriptInvocationContext = new ScriptInvocationContext()
+            {
+                ExecutionContext = new ExecutionContext()
+                {
+                    InvocationId = invocationId,
+                    FunctionName = functionName,
+                },
+                BindingData = GetScriptInvocationBindingData(),
+                Inputs = GetScriptInvocationInputs(),
+                ResultSource = new TaskCompletionSource<ScriptInvocationResult>(),
+                Logger = testLogger,
+                AsyncExecutionContext = System.Threading.ExecutionContext.Capture()
+            };
+            var functionMetadata = new FunctionMetadata
+            {
+                Name = functionName
+            };
+            var httpTriggerBinding = new BindingMetadata
+            {
+                Name = "req",
+                Type = "httpTrigger",
+                Direction = BindingDirection.In,
+                Raw = new JObject()
+            };
+            var queueInputBinding = new BindingMetadata
+            {
+                Name = "queueInput",
+                Type = "queue",
+                Direction = BindingDirection.In
+            };
+            var httpOutputBinding = new BindingMetadata
+            {
+                Name = "res",
+                Type = "http",
+                Direction = BindingDirection.Out,
+                Raw = new JObject()
+            };
+            scriptInvocationContext.FunctionMetadata = functionMetadata;
+            return scriptInvocationContext;
+        }
+
+        public static ScriptInvocationContext GetSimpleHttpTriggerScriptInvocationContext(string functionName, Guid invocationId, ILogger testLogger)
+        {
+            ScriptInvocationContext scriptInvocationContext = new ScriptInvocationContext()
+            {
+                ExecutionContext = new ExecutionContext()
+                {
+                    InvocationId = invocationId,
+                    FunctionName = functionName,
+                },
+                BindingData = GetScriptInvocationBindingData(),
+                Inputs = GetSimpleHttpTriggerScriptInvocationInputs(),
+                ResultSource = new TaskCompletionSource<ScriptInvocationResult>(),
+                Logger = testLogger,
+                AsyncExecutionContext = System.Threading.ExecutionContext.Capture()
+            };
+            var functionMetadata = new FunctionMetadata
+            {
+                Name = functionName
+            };
+            var httpTriggerBinding = new BindingMetadata
+            {
+                Name = "req",
+                Type = "httpTrigger",
+                Direction = BindingDirection.In,
+                Raw = new JObject()
+            };
+            var httpOutputBinding = new BindingMetadata
+            {
+                Name = "res",
+                Type = "http",
+                Direction = BindingDirection.Out,
+                Raw = new JObject()
+            };
+            functionMetadata.Bindings.Add(httpTriggerBinding);
+            functionMetadata.Bindings.Add(httpOutputBinding);
+            scriptInvocationContext.FunctionMetadata = functionMetadata;
+            return scriptInvocationContext;
+        }
+    }
+}
